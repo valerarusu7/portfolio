df1042e45ce9e82ee566eeebafc9dfa9d9d4595e
Valeriu Rusu <280167@via.dk>
2022-03-11 00:30:51 +0100

    Social links added

--- src/components/Header/HeaderSocials.tsx
+++ src/components/Header/HeaderSocials.tsx
@@ -5,13 +5,13 @@ import { FaGithub } from "react-icons/fa";
 function HeaderSocials() {
   return (
     <div className="header__socials">
-      <a href="linkdin" target="_blank">
+      <a href="https://www.linkedin.com/in/valeriu-rusu-878043180" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="github" target="_blank">
+      <a href="https://github.com/valerarusu7" target="_blank">
         <FaGithub />
       </a>
-      <a href="facebook" target="_blank">
+      <a href="https://www.facebook.com/valera.rusu111" target="_blank">
         <BsFacebook />
       </a>
     </div>
