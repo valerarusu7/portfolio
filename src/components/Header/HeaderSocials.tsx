21c8b7dd87ad4089463b386bffe2133020f57aa1
Valeriu Rusu <280167@via.dk>
2022-03-11 18:06:29 +0100

    Fix social links

--- src/components/Header/HeaderSocials.tsx
+++ src/components/Header/HeaderSocials.tsx
@@ -5,13 +5,13 @@ import { FaGithub } from "react-icons/fa";
 function HeaderSocials() {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/valeriu-rusu-878043180" target="_blank">
+      <a href="https://www.linkedin.com/in/valeriu-rusu-878043180" rel="noreferrer" target="_blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com/valerarusu7" target="_blank">
+      <a href="https://github.com/valerarusu7" rel="noreferrer" target="_blank">
         <FaGithub />
       </a>
-      <a href="https://www.facebook.com/valera.rusu111" target="_blank">
+      <a href="https://www.facebook.com/valera.rusu111" rel="noreferrer" target="_blank">
         <BsFacebook />
       </a>
     </div>
